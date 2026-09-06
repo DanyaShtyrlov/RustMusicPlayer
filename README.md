e7529f99730134e1abef422059a2c91838d19363
unknown <danya.shtyrlov@yandex.ru>
2026-09-06 16:03:04 +0300

    Added scrollable list of songs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#RustMusicPlayer
+# RustMusicPlayer
 
-> Self written local music player using rodio and iced **Rust** libraries
+> Self written local music player using rodio and iced **Rust** libraries. 
 > This project also aimed for practicing git working process
 
 **Thank you for visiting**
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use iced::widget::{Column, button, row, text};
+use iced::widget::{Column, button, row, scrollable, text};
 use iced::window::Position;
-use iced::{Element, Size, window};
+use iced::{Element, Length, Size, window};
 use std::fs;
 
 struct Player {
     list: Vec<String>,
+    selected_song: Option<usize>,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
     Play,
+    SelectSong(usize),
 }
 
 impl Default for Player {
     fn default() -> Self {
         let mut list = Vec::new();
-        list.clear();
         if let Ok(dir) = fs::read_dir(r"D:\rust_projects\player\examples") {
             for entry in dir.flatten() {
                 let path = entry.path();
@@ -25,26 +26,45 @@ impl Default for Player {
             }
         }
 
-        println!("{:?}", list);
-        Self { list }
+        Self {
+            list,
+            selected_song: None,
+        }
     }
 }
 
 impl Player {
-    fn update(&mut self, _message: Message) {}
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::SelectSong(index) => {
+                self.selected_song = Some(index);
+            }
+            Message::Play => todo!(),
+        }
+    }
 
     fn view(&self) -> Element<'_, Message> {
         let play_button = button("Play").on_press(Message::Play);
         let elements: Vec<Element<Message>> = self
             .list
             .iter()
-            .map(|item| text(item).size(14).into())
+            .enumerate()
+            .map(|(index, item)| {
+                let is_selected = self.selected_song == Some(index);
+                let item_button = button(text(item).size(15))
+                    .width(Length::Fill)
+                    .style(if is_selected {
+                        button::primary
+                    } else {
+                        button::secondary
+                    })
+                    .on_press(Message::SelectSong(index));
+                row![item_button].spacing(8).into()
+            })
             .collect();
-        let list_column = Column::with_children(elements);
-        row![play_button, list_column]
-            .spacing(10)
-            .padding(20)
-            .into()
+        let list_view = scrollable(Column::with_children(elements).spacing(6)).height(Length::Fill);
+        //let list_column = Column::with_children(elements);
+        row![play_button, list_view].spacing(10).padding(20).into()
     }
 }
 
@@ -72,7 +92,7 @@ fn main() -> iced::Result {
     iced::application(Player::default, Player::update, Player::view)
         .title("Rust Music Player")
         .window(window::Settings {
-            size: Size::new(800.0, 600.0),
+            size: Size::new(400.0, 600.0),
             fullscreen: false,
             position: Position::Centered,
             resizable: false,
